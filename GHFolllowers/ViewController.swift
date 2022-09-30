4866b7ded78aadd5c91fd6dd987cf5c8019b0ce9
Ruslan Ismayilov <70414587+Rusi0991@users.noreply.github.com>
2022-09-29 17:34:38 -0700

    GHFollowers project created

---
+++ GHFolllowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFolllowers
+//
+//  Created by Ruslan Ismayilov on 9/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
