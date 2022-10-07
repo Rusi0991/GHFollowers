50073273ae66ca4d38b18c685a283e65cf11c056
Ruslan Ismayilov <70414587+Rusi0991@users.noreply.github.com>
2022-10-06 17:33:15 -0700

    Added NetworkManager, network call to get followers using func getFollowers

--- GHFolllowers/Screens/FollowerListVC.swift
+++ GHFolllowers/Screens/FollowerListVC.swift
@@ -16,6 +16,16 @@ class FollowerListVC: UIViewController {
         view.backgroundColor = .systemBackground
         
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
+            guard let followers = followers else {
+                self.presentGFAlertOnMAinThread(title: "Bad stuff happened", message: errorMessage!, buttonTitle: "OK")
+                return
+            }
+            print("Followers.count = \(followers.count)")
+            print(followers)
+        }
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
