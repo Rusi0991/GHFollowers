161bf2e6e5b7685122f3216de0c5121d427e66d0
Ruslan Ismayilov <70414587+Rusi0991@users.noreply.github.com>
2022-10-27 18:38:57 -0700

    GFSecondaryBodyLabel file added

---
+++ GHFolllowers/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
@@ -0,0 +1,21 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  GHFolllowers
+//
+//  Created by Ruslan Ismayilov on 10/27/22.
+//
+
+import UIKit
+
+class GFUserInfoHeaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+   
+
+}
