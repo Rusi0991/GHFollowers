38cb0c4cd57f2b038f123377a4aad49db1b6c689
Ruslan Ismayilov <70414587+Rusi0991@users.noreply.github.com>
2022-11-01 23:55:20 -0700

    GFUserInfoHeaderVC added

---
+++ GHFolllowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFolllowers
+//
+//  Created by Ruslan Ismayilov on 11/1/22.
+//
+
+import Foundation
+
+
+enum SFSymbols {
+static let location = "mappin.and.ellipse"
+}
